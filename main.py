ea47c29fa2e97094cabd9c2693263b4ca72e2338
Jesper Melin <jesper.melin87@gmail.com>
2019-07-10 14:39:53 +0200

    Usage of abstract sources added to main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Runs the main game-loop.
 """
 
 # Imports
+import os
 import sys
 import abc
 import numpy as np
@@ -13,116 +14,35 @@ from random import seed
 from random import randint
 from PIL import Image
 from pathlib import Path
+from helpers import ImageHelper
 
 # Global variables
-
-
-class ImageHelper():
-    @staticmethod
-    def convert_rgba_to_rgb(image, color=(0, 0, 0)):
-        background = Image.new('RGB', image.size, color)
-        background.paste(image, mask=image.split()[3])
-        return background
-
-    @staticmethod
-    def convert_any_to_rgb(image):
-        bands = image.getbands()
-        if bands == ('R', 'G', 'B', 'A'):
-            image = ImageHelper.convert_rgba_to_rgb(image)
-        elif bands != ('R', 'G', 'B'):
-            image = image.convert('RGB')
-        return image
-
-    @staticmethod
-    def resize_image(image, size):
-        if image.size != size:
-            image = image.resize(size)
-        return image
+FPS = 60
 
 # Class declarations
-
-
-class SpriteViewer():
-    def __init__(self, width=16, height=16):
-        self.width = width
-        self.height = height
-        self._buffer = np.zeros((self.height, self.width, 3), dtype=np.uint8)
-        self.images_rgb_data = []
-        self.number_of_frames = 0
-        self.current_frame = 0
-        self.duration = 0
-        self.totalTime = 0.
-        return
-
-    def load_image(self, filename):
-        im = Image.open(filename)
-
-        im = ImageHelper.convert_any_to_rgb(im)
-        im = ImageHelper.resize_image(im, (self.width, self.height))
-
-        self._buffer = np.array(im)
-
-    def image_as_array(self):
-        return self._buffer
-
-    def update(self, dt):
-
-        self.totalTime += dt
-        if(self.totalTime > self.duration):
-            self.current_frame += 1
-            self.totalTime = 0
-        
-        self._buffer = self.images_rgb_data[self.current_frame % self.number_of_frames]
-        return self._buffer
-
-    def load_gif(self, filename, repetitions=1):
-        try:
-            im = Image.open(filename)
-        except IOError:
-            print("{} is not a valid image file".format(filename))
-
-        try:
-            # The time to display the current frame of the GIF, in milliseconds
-            self.duration = float(im.info["duration"]) / 1000
-        except KeyError:
-            print("{} has no duration meta data!".format(filename))
-        except (TypeError, ValueError):
-            print("cannot convert info[duration]: {} to integer.".format(
-                im.info["duration"]))
-        except:
-            print("Unknown error while loading {}".format(filename))
-            return
-
-        more_frames = True
-
-        while more_frames:
-            frame = Image.new("RGBA", im.size)
-            frame.paste(im)
-            frame = ImageHelper.convert_any_to_rgb(frame)
-            frame = ImageHelper.resize_image(frame, (self.width, self.height))
-            self.images_rgb_data.append(np.array(frame))
-            try:
-                im.seek(im.tell() + 1)
-            except:
-                more_frames = False
-            self.number_of_frames += 1
-
-
 class RetroFrame():
     def __init__(self):
         # Create surface of (width, height), and its window.
         from computer import Computer
+        from animation_source import AnimationSource
+        from image_source import ImageSource
+        from sprite_source import SpriteSource
+        
+        # Working directories
+        image_source = os.path.join(os.path.dirname(__file__),'/retroframe/images/')
+        gif_to_load = os.path.join(image_source,'_1.gif')
+        image_to_load = os.path.join(image_source,'1.png')
+        sprite_to_load = os.path.join(image_source,'sprite_mario.png')
+        mario_sprite = os.path.join(image_source,'characters.gif')
+
         self.display = Computer(16, 16)
-        self.sprite = SpriteViewer(16, 16)
+        self.image = ImageSource(image_to_load, 16, 16)
+        self.animation = AnimationSource(gif_to_load, 16, 16)
+        self.sprite = SpriteSource(sprite_to_load, 233, 99, 16, 16, 5, 0.2, 1, 1)
+        self.goomba = SpriteSource(mario_sprite, 296, 187, 16, 16, 2, 0.2, 3, 3)
         # Set up some data
 
     def mainloop(self):
-        seed(1)
-        image_folder = Path("images").absolute()
-        # image_folder / "_1.gif"
-        file_to_load = (r'F:\PC_BACKUP\Users\Jesper\Documents\GitHub\retro-frame\images\_1.gif')
-        self.sprite.load_gif(file_to_load)
-        FPS = 60
         lastFrameTime = time.time()
 
         while True:
@@ -132,7 +52,11 @@ class RetroFrame():
             lastFrameTime = currentTime
 
             # Update game logic, objects and data structures here using dt
-            self.display.buffer = self.sprite.update(dt)
+            self.sprite.update(dt)
+            self.animation.update(dt)
+            self.image.update(dt)
+            self.goomba.update(dt)
+            self.display.buffer = self.goomba.buffer
             
             #for i in range(self.display.number_of_pixels):
             #    self.display.set_pixel_at_index(i, (randint(0, 255), randint(0, 255), randint(0, 255)))
