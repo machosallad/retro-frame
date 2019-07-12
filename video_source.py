3c84277e84e40f63830cc62c7b35921ce14c730a
Jesper Melin <jesper.melin87@gmail.com>
2019-07-12 18:35:52 +0200

    Video source properly supported - Rescaled using opencv methods - Color space changed from BRG to RGB - Number of frames correctly stored

--- video_source.py
+++ video_source.py
@@ -12,36 +12,58 @@ class VideoSource(AbstractSource):
     def __init__(self, filename, width=16, height=16):
         super().__init__(width, height)
 
-        self.video_frames = []
-        self.load_videos(filename)
         self.current_frame = 0
         self.number_of_frames = 0
-
-    def load_videos(self,video_file):
-        # print "load_videos"
-        vid_frames = []
-        capture = cv2.VideoCapture(video_file)
-
-        read_flag, frame = capture.read()
-        i = 1
-
-        while (read_flag):
-            # print i
-            if i % 10 == 0:
-                self.video_frames.append(frame)
-                vid_frames.append(frame)
-            
-            read_flag, frame = capture.read()
-            i += 1
-        vid_frames = np.asarray(vid_frames, dtype='uint8')[:-1]
-        capture.release()
-        self.number_of_frames = i
-        print (i)
+        self.total_time = 0
+        self.video_frames = []
+        self.frame_rate = 0
+        self.load(filename)
 
     def load(self, filename):
-        pass
+        print ("Loading video: {0}".format(filename))
+        video = cv2.VideoCapture(filename)
+
+        # Find OpenCV version
+        (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+      
+        if int(major_ver)  < 3 :
+            self.frame_rate = video.get(cv2.cv.CV_CAP_PROP_FPS)
+        else:
+            self.frame_rate = video.get(cv2.CAP_PROP_FPS)
+
+        print ("Frames per second : {0}".format(self.frame_rate))
+
+        # Start capturing the video frames
+        read_flag, frame = video.read()
+        i = 1
+        frame_counter = 0
+
+        start_time = time.time()
+        while (read_flag):
+            # print i
+            if i % 1 == 0:
+                frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                frame = cv2.resize(frame, (16,16), interpolation = cv2.INTER_AREA )
+                self.video_frames.append(frame)
+                frame_counter += 1          
+            read_flag, frame = video.read()
+            i += 1
+        video.release()
+        stop_time = time.time()
+        print("Elapsed time to capture video frames:{0}".format(stop_time-start_time))
+        
+        self.number_of_frames = frame_counter
+        print ("Number of frames captures:{0}".format(self.number_of_frames))
 
     def update(self, dt):
-        self._buffer = self.video_frames[5]
-        self.current_frame += 1
-        print(self.current_frame)
+        self.total_time += dt
+        if(self.total_time > (1./self.frame_rate)):
+            self.current_frame += 1
+            self.total_time = 0
+
+        # Restore the frame counter
+        if self.current_frame >= self.number_of_frames:
+            self.current_frame = 0
+
+        self._buffer = self.video_frames[self.current_frame]
+        
